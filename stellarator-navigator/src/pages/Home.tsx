3cf19e5489e34e9fdbd2977c46fb36509049b0d2
Jeff Soules <jsoules@flatironinstitute.org>
2023-10-27 17:16:56 -0400

    Improve styling for cites.

--- stellarator-navigator/src/pages/Home.tsx
+++ stellarator-navigator/src/pages/Home.tsx
@@ -9,6 +9,28 @@ import Overview from './Overview'
 
 const BASENAME = import.meta.env.BASE_URL
 
+const articles: {link: string, title: string}[] = [
+    {
+        link: "https://doi.org/10.1016/j.jcp.2022.111147",
+        title: "A. Giuliani, F. Wechsung, A. Cerfon, G. Stadler, " +
+                "M. Landreman, Single-stage gradient-based stellarator coil " +
+                "design: Optimization for near-axis quasi-symmetry. Journal of " +
+                "Computational Physics, 459 (2022), 111147.",},
+    {
+        link: "https://doi.org/10.1017/S0022377822000563",
+        title: "Giuliani, A., Wechsung, F., Stadler, G., Cerfon, A., & Landreman, " +
+               "M. (2022). Direct computation of magnetic surfaces in Boozer coordinates " +
+               "and coil optimization for quasisymmetry. Journal of Plasma Physics, 88 (4), 905880401."
+    },
+    {
+        link: "https://doi.org/10.1063/5.0129716",
+        title: "Andrew Giuliani, Florian Wechsung, " +
+               "Antoine Cerfon, Matt Landreman, Georg Stadler; " +
+               "Direct stellarator coil optimization for nested magnetic " +
+               "surfaces with precise quasi-symmetry. Phys. Plasmas 1 April 2023; 30 (4): 042511."
+    },
+]
+
 const Home: FunctionComponent = () => {
     // TODO: Margin, more styling, etc.
     const { database } = useContext(NavigatorContext)
@@ -70,20 +92,12 @@ const Home: FunctionComponent = () => {
                 <div>
                     QUASR database by Andrew Giuliani (Flatiron Institute), based on algorithms
                     developed with Florian Wechsung (NYU), Georg Stadler (NYU), Antoine Cerfon (NYU),
-                    and Matt Landreman (U Maryland), described in
-                    (
-                    <a
-                        href="https://doi.org/10.1016/j.jcp.2022.111147"
-                        title="A. Giuliani, F. Wechsung, A. Cerfon, G. Stadler, M. Landreman, Single-stage gradient-based stellarator coil design: Optimization for near-axis quasi-symmetry. Journal of Computational Physics, 459 (2022), 111147."
-                    >1</a>), (
-                    <a
-                        href="https://doi.org/10.1017/S0022377822000563"
-                        title="Giuliani, A., Wechsung, F., Stadler, G., Cerfon, A., & Landreman, M. (2022). Direct computation of magnetic surfaces in Boozer coordinates and coil optimization for quasisymmetry. Journal of Plasma Physics, 88 (4), 905880401."
-                    >2</a>), (
-                    <a
-                        href="https://doi.org/10.1063/5.0129716"
-                        title="Andrew Giuliani, Florian Wechsung, Antoine Cerfon, Matt Landreman, Georg Stadler; Direct stellarator coil optimization for nested magnetic surfaces with precise quasi-symmetry. Phys. Plasmas 1 April 2023; 30 (4): 042511."
-                    >3</a>)
+                    and Matt Landreman (U Maryland), and described in
+                    {articles.map((a, i) => (
+                        <Tooltip title={a.title}>
+                            <span>&nbsp;(<a href={a.link}>{i + 1}</a>)</span>
+                        </Tooltip>
+                    ))}
                 </div>
                 <div>
                     Stellarator Navigator by Jeff Soules (Flatiron Institute)
